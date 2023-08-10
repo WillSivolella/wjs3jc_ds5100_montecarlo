6109869471adc66dfe9823eb5b448bb2be902595
Will Sivolella <114835947+WillSivolella@users.noreply.github.com>
2023-08-10 11:35:23 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'montecarlo',
+    version = '0.1.0',
+    url = 'https://github.com/WillSivolella/wjs3jc_ds5100_montecarlo.git',
+    author = 'Will Sivolella',
+    author_email = 'wjs3jc@virginia.edu',
+    packages = find.packages(),
+    install_requires = ['pandas >= 0.2' ]
+)
